26b4a4cbfee3f562e8a6b364d05f6475ddc408df
Celena Moscovich <celena.ark@gmail.com>
2023-10-06 11:48:36 -0300

    nombre del controladoooor

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once("config/Configuracion.php");
 $configuracion = new Configuracion();
 $router = $configuracion->getRouter();
 
-$controller = $_GET['controller'] ?? "home";
+$controller = $_GET['controller'] ?? "AnimaliaController";
 $method = $_GET['method'] ?? 'mostrarPantallaInicial';
 
 $router->route($controller, $method);
