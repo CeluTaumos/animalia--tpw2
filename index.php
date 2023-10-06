0185fce6ff757edab8c196415d1118eecf78285d
Celena Moscovich <celena.ark@gmail.com>
2023-10-06 20:06:07 -0300

    DEMENCIAAA

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 session_start();
 include_once("config/Configuracion.php");
 $configuracion = new Configuracion();
@@ -8,4 +10,6 @@ $router = $configuracion->getRouter();
 // $method = $_GET['method'] ?? 'mostrarPantallaInicial';
 
 $router->route("Animalia","mostrarPantallaInicial");
+
+
 ?>
