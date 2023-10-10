35beb8c9a0589233d7cbe3b15df48486f455eb6a
Axel Daniel <axeelleguero@gmail.com>
2023-10-10 18:21:06 -0300

    noo

--- controller/AnimaliaController.php
+++ controller/AnimaliaController.php
@@ -18,16 +18,21 @@ class AnimaliaController {
     public function procesarFormulario() {
 
         //LOGICA PARA DESPUES
-//     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//         $nombre = $_POST["nombre"];
-//         $fecha = $_POST["fecha"];
-//         $sexo = $_POST["sexo"];
-
+        // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        //     $nombre = $_POST["nombre"];
+        //     $fecha = $_POST["fecha"];
+        //     $sexo = $_POST["sexo"];
+        //     $pais = $_POST["pais"];
+        //     $ciudad = $_POST["ciudad"];
+        //     $user = $_POST["user"];
+        //     $pass = $_POST["pass"];
+        //     $pass2 = $_POST["pass2"];
+        // } 
 //         echo "Nombre: " . $nombre . "<br>";
 //         echo "Fecha: " . $fecha . "<br>";
 //         echo "Sexo: " . $sexo . "<br>";
 // }
-$datos =null;
+        $datos =null;
         $this->render->printView('registroExitoo', $datos);
 }   
 }
