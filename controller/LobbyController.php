45fe54451e4ea886c52595dfd81e27869a170876
Celena Moscovich <celena.ark@gmail.com>
2023-10-24 20:41:41 -0300

    que no se repitan preguntas

--- controller/LobbyController.php
+++ controller/LobbyController.php
@@ -1,19 +1,30 @@
 <?php
 
-class LobbyController {
+class LobbyController
+{
     private $render;
     private $model;
 
-    public function __construct($render, $model) {
+    public function __construct($render, $model)
+    {
         $this->render = $render;
         $this->model = $model;
     }
-    public function mandarCorreo(){
+    public function mandarCorreo()
+    {
         //Utiliza libreria phpmailer para enviar correo
     }
 
-    public function mostrarPantallaLobby() {
-        $datos =null;
+    public function mostrarPantallaLobby()
+    {
+        $datos = null;
         $this->render->printView('lobby', $datos);
-}   
+    }
+
+    public function cerrarSesion()
+    {
+        $datos = null;
+        session_destroy();
+        $this->render->printView('index', $datos);
+    }
 }
