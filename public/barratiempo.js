fcaf371dbf5ed3965e9847f7b079083b99e47b58
Celena Moscovich <celena.ark@gmail.com>
2023-10-27 15:21:18 -0300

    barra de tiempo

---
+++ public/barratiempo.js
@@ -0,0 +1,31 @@
+
+let tiempoInicial = 60;
+let tiempoRestante = tiempoInicial;
+const barraTiempo = document.getElementById("barraTiempo");
+const tiempoRestanteSpan = document.getElementById("tiempoRestante");
+
+
+function actualizarBarraTiempo() {
+  const porcentajeTiempoRestante = (tiempoRestante / tiempoInicial) * 100;
+  barraTiempo.style.width = porcentajeTiempoRestante + "%";
+}
+
+function actualizarContadorTiempo() {
+  tiempoRestanteSpan.textContent = tiempoRestante + " segundos";
+}
+
+
+function reducirTiempo() {
+  if (tiempoRestante > 0) {
+    tiempoRestante--;
+    actualizarBarraTiempo();
+    actualizarContadorTiempo();
+  }
+}
+
+
+const interval = setInterval(reducirTiempo, 1000);
+
+
+actualizarBarraTiempo();
+actualizarContadorTiempo();
