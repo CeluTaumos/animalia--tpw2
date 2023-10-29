fa99743620d40360dba674f8635e4f6813ac460c
LudmiAbril <ludmila.pereyra543@gmail.com>
2023-10-29 13:13:00 -0300

    mas preguntas de ciencia

--- config/juego_animalia.sql
+++ config/juego_animalia.sql
@@ -59,7 +59,7 @@ insert into categoria(id, tipo, imagen)values(1, "Historia", "config/images/hist
 insert into dificultad(id, nombre)values(1, "principiante"), (2, "intermedio"), (3, "avanzado");
 
 -- primeras preguntas de prueba con sus respuestas
-insert into pregunta (id, descripcion, categoria) values (1, "¿Cuál es el mamífero más grande?", 5), (2, "¿Qué animal es conocido por su caparazón de placas y se encuentra en el Gran Chaco?", 5), (3, "¿Cuál es el ave más grande de Argentina?", 5), (4, "¿Cuál de estos animales es endémico de la región patagónica argentina?", 5);
+insert into pregunta (id, descripcion, categoria,dificultad) values (1, "¿Cuál es el mamífero más grande?", 5,1), (2, "¿Qué animal es conocido por su caparazón de placas y se encuentra en el Gran Chaco?", 5,1), (3, "¿Cuál es el ave más grande de Argentina?", 5,1), (4, "¿Cuál de estos animales es endémico de la región patagónica argentina?", 5,1);
 
 insert into respuesta (id, descripcion, es_correcta, pregunta) values (1, "La ballena azul", true, 1), (2, "El bicho palo", false, 1), (3, "Elon Musk", false, 1), (4, "Nemo", false, 1);
 
@@ -69,42 +69,43 @@ insert into respuesta (id, descripcion, es_correcta, pregunta) values (9, "El pi
 
 insert into respuesta (id, descripcion, es_correcta, pregunta) values (13, "El guanaco", true, 4), (14, "El yacaré", false, 4), (15, "El coatí", false, 4), (16, "El caballo", false, 4);
 
-/*preguntas de geo*/
-insert into pregunta (id, descripcion, categoria) values 
-(5, "¿Cuál es el punto más alto de Argentina y de toda América del Sur, y en qué provincia se encuentra?", 4), 
-(6, "¿En qué provincia argentina se encuentra el desierto conocido como El Gran Seco?", 4), 
-(7, "¿Cuál es la ciudad más grande de la Patagonia argentina?", 4), 
-(8, "¿Cuál es la longitud del río Paraná?", 4);
+/*preguntas de geo principiante*/
 
-/*preguntas de deportes*/
-insert into pregunta (id, descripcion, categoria) values 
-(9, "¿En qué año obtuvo la medalla de oro Delfina Pignatiello?", 3), 
-(10, "¿Cuál es el deporte nacional de Argentina?", 3), 
-(11, "¿De qué signo es Messi?", 3), 
-(12, "¿Cuál es el primer campeón mundial de box argentino?", 3),
-(13, "¿A qué edad obtuvo el título de Gran Maestra Femenina de ajedrez Candela Belén Francisco Guecamburu?", 3),
-;
+insert into pregunta (id, descripcion, categoria, dificultad) values 
+(5, "¿Cuál es el punto más alto de Argentina y de toda América del Sur, y en qué provincia se encuentra?", 4,1), 
+(6, "¿En qué provincia argentina se encuentra el desierto conocido como El Gran Seco?", 4,1), 
+(7, "¿Cuál es la ciudad más grande de la Patagonia argentina?", 4,1), 
+(8, "¿Cuál es la longitud del río Paraná?", 4,1);
 
-/*pregunta de cultura*/
-insert into pregunta (id, descripcion, categoria) values
-(14, "¿En qué año se realizó la última gira de Soda Stereo?", 2), 
-(15, "¿Cuál es la icónica frase de Moria Casán?", 2), 
-(16, "¿En qué año falleció Ricardo Fort?", 2),
-(17, "¿Quién dijo 'Detrás del humo no se ve, no, no se ve'?", 2);
+/*preguntas de deportes principiante*/
+insert into pregunta (id, descripcion, categoria, dificultad) values 
+(9, "¿En qué año obtuvo la medalla de oro Delfina Pignatiello?", 3,1), 
+(10, "¿Cuál es el deporte nacional de Argentina?", 3,1), 
+(11, "¿De qué signo es Messi?", 3,1), 
+(12, "¿Cuál es el primer campeón mundial de box argentino?", 3,1),
+(13, "¿A qué edad obtuvo el título de Gran Maestra Femenina de ajedrez Candela Belén Francisco Guecamburu?", 3,1);
 
-/*preguntas de historia*/
-insert into pregunta (id, descripcion, categoria) values 
-(18, "¿Cuál fue el nombre del primer presidente de Argentina, que asumió el cargo en 1862?", 1), 
-(19, "¿En qué año se promulgó la Constitución Nacional de Argentina?", 1), 
-(20, "¿Quién lideró la Revolución de Mayo en 1810?", 1), 
-(21, "¿En qué año se promulgó la Ley de Divorcio en Argentina?", 1);
+/*pregunta de cultura principiante*/
+insert into pregunta (id, descripcion, categoria, dificultad) values
+(14, "¿En qué año se realizó la última gira de Soda Stereo?", 2,1), 
+(15, "¿Cuál es la icónica frase de Moria Casán?", 2,1), 
+(16, "¿En qué año falleció Ricardo Fort?", 2,1),
+(17, "¿Quién dijo 'Detrás del humo no se ve, no, no se ve'?", 2,1),
+(22, "¿De qué murió Favaloro?", 2,1);
 
-insert into Pregunta (id, descripcion, categoria) values (22, "¿De qué murió Favaloro?", 2);
-insert into respuesta (id, descripcion, es_correcta, pregunta) values 
-(85, "Suicidio", true, 22), 
-(86, "Se comió un pez globo", false, 22), 
-(87, "Murió en el atentado a las torres gemelas", false, 22), 
-(88, "Lo balearon", false, 22);
+/*preguntas de historia principiante*/
+insert into pregunta (id, descripcion, categoria, dificultad) values 
+(18, "¿Cuál fue el nombre del primer presidente de Argentina, que asumió el cargo en 1862?", 1,1), 
+(19, "¿En qué año se promulgó la Constitución Nacional de Argentina?", 1,1), 
+(20, "¿Quién lideró la Revolución de Mayo en 1810?", 1,1), 
+(21, "¿En qué año se promulgó la Ley de Divorcio en Argentina?", 1,1); 
+
+/* preguntas de ciencia principiante*/
+insert into pregunta (id, descripcion, categoria, dificultad) values (23,"¿Cuál de los siguientes científicos argentinos es conocido por sus contribuciones a la medicina y la cirugía cardiovascular?",5,1),
+(24, "¿Cuál es el organismo argentino encargado de la investigación y desarrollo en el campo de la ciencia y tecnología?",5,1),
+(25,"¿Cuál es el nombre de la agencia espacial argentina encargada de las actividades espaciales y satelitales?",5,1),
+(26,"¿Cuál es el nombre del científico argentino famoso por sus investigaciones sobre la penicilina y la insulina?",5,1);
+
 /*respuestas*/
 insert into respuesta (id, descripcion, es_correcta, pregunta) values 
 (17, "El Aconcagua", true, 5), 
@@ -208,8 +209,34 @@ insert into respuesta (id, descripcion, es_correcta, pregunta) values
 (83, "1992", false, 21),
 (84, "2000", false, 21);
 
-update pregunta
-set descripcion = "Cortala, pipo"
-where id = 58;
+insert into respuesta (id, descripcion, es_correcta, pregunta) values 
+(85, "Suicidio", true, 22), 
+(86, "Se comió un pez globo", false, 22), 
+(87, "Murió en el atentado a las torres gemelas", false, 22), 
+(88, "Lo balearon", false, 22);
 
+insert into respuesta (id, descripcion, es_correcta, pregunta) values 
+(89, "René Favaloro", true, 23), 
+(90, "César Milstein", false, 23), 
+(91, "Margarita Salas", false, 23), 
+(92, "Peter Capusotto", false, 23);
 
+insert into respuesta (id, descripcion, es_correcta, pregunta) values 
+(93, "CONICET", true, 24), 
+(94, "ANMAT", false, 24), 
+(95, "AFIP", false, 24), 
+(96, "MGM", false, 24);
+
+insert into respuesta (id, descripcion, es_correcta, pregunta) values 
+(97, "CONAE", true, 25), 
+(98, "CONALEP", false, 25), 
+(99, "INVAP", false, 25), 
+(100, "NASA", false, 25);
+
+insert into respuesta (id, descripcion, es_correcta, pregunta) values 
+(101, "Luis Federico Leloir", true, 26), 
+(102, "Juan Domingo Perón", false, 26), 
+(103, "Jorge Luis Borges", false, 26), 
+(104, "Juan Carlos Altavista", false, 26);
+
+-- preguntas geo intermedio
