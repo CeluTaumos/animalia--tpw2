d3c2ed74e52520fc243b47c26228bd680ba249ea
Celena Moscovich <celena.ark@gmail.com>
2023-11-03 09:49:03 -0300

    controlador con verificacion de trampas y demas

--- controller/PartidaController.php
+++ controller/PartidaController.php
@@ -24,7 +24,115 @@ class PartidaController
     }
 
 
+   
     public function mostrarPantallaPartida()
+    {
+        // Verificar si la partida ha terminado
+        if ($_SESSION['puntaje'] < 0) {
+            $this->terminarPartida();
+            return;
+        }
+
+        $idGenerado = $this->getPregunta();
+
+        if (!$this->verificarPreguntaRepetida()) {
+            $_SESSION['preguntas_mostradas'][] = $idGenerado;
+
+            $datos['pregunta'] = $this->model->getPreguntaPorID($idGenerado);
+            $datos['respuesta'] = $this->model->getRespuestaPorID($idGenerado);
+
+            // Generar una clave única para esta pregunta
+            $pregunta_id = uniqid();
+
+            // Guardar el tiempo de inicio en la sesión
+            $_SESSION['tiempo_inicio'][$pregunta_id] = time();
+
+            $this->render->printView('jugarPartida', $datos);
+        } else {
+            // La pregunta se repite, mostrar otra pregunta
+            $this->mostrarPantallaPartida();
+        }
+    }
+
+
+
+    public function pantallaPerdedor()
+    {
+        $datos['puntaje'] = $_SESSION['puntaje'];
+        $this->render->printView('pantallaPerdedor', $datos);
+    }
+
+
+
+    public function verificarRespuesta() {
+    if ($this->verificarTrampa()) {
+        // El usuario intentó hacer trampa
+        $this->terminarPartida();
+        return;
+    }
+
+    $datos = array();
+
+    if (isset($_POST['id'])) {
+        $id = $_POST['id'];
+        $respuesta = $this->model->getRespuesta($id);
+
+        if ($respuesta) {
+            $esCorrecta = (int)$respuesta[0]['respuesta_correcta'];
+            if ($esCorrecta === 1) {
+                if (!$this->verificarTiempoLimite()) {
+                    // El usuario respondió correctamente, pero se pasó del tiempo límite
+                    $this->terminarPartida();
+                    return;
+                }
+
+                // Respuesta correcta y dentro del tiempo límite
+                $this->puntaje++;
+                $_SESSION['puntaje'] += $this->puntaje;
+
+                $usuario = $_SESSION['user'];
+                $id = $this->model->getIdPartida($usuario);
+
+                if ($id !== null) {
+                    $this->model->aumentarPuntuacionEnPartida($usuario, $id);
+                } else {
+                    $this->model->subirPuntuacionEnPartida($usuario);
+                }
+
+                $this->mostrarPantallaPartida();
+            } else {
+                // Respuesta incorrecta
+                $this->terminarPartida();
+            }
+        }
+    }
+}
+
+    private function terminarPartida()
+    {
+        $datos['puntaje'] = $_SESSION['puntaje'];
+        $this->render->printView('pantallaPerdedor', $datos);
+
+        // Reiniciar el puntaje y limpiar las variables de sesión relacionadas con la partida
+        $_SESSION['puntaje'] = 0;
+        unset($_SESSION['pregunta_actual']);
+        unset($_SESSION['tiempo_inicio']);
+        unset($_SESSION['ultima_pregunta_mostrada']);
+    }
+
+    private function verificarTrampa()
+    {
+        if (isset($_SESSION['ultima_pregunta_mostrada'])) {
+            $tiempo_actual = time();
+            $tiempo_transcurrido = $tiempo_actual - $_SESSION['ultima_pregunta_mostrada'];
+            if ($tiempo_transcurrido < 10) {
+                return true; // El usuario intentó hacer trampa recargando la página antes de tiempo
+            }
+        }
+        return false;
+    }
+
+    private function verificarPreguntaRepetida()
     {
         if (!isset($_SESSION['preguntas_mostradas'])) {
             $_SESSION['preguntas_mostradas'] = array();
@@ -33,6 +141,30 @@ class PartidaController
         if (empty($_SESSION['preguntas_disponibles'])) {
             $preguntas = $this->model->getPreguntas();
             $_SESSION['preguntas_disponibles'] = array_column($preguntas, 'id');
+        }
+
+        $preguntas_disponibles = $_SESSION['preguntas_disponibles'];
+        $idGenerado = $this->getPregunta();
+
+        return in_array($idGenerado, $_SESSION['preguntas_mostradas']);
+    }
+
+    private function verificarTiempoLimite()
+    {
+        if (isset($_SESSION['tiempo_inicio'][$_SESSION['pregunta_actual']])) {
+            $tiempo_actual = time();
+            $tiempo_inicio_pregunta = $_SESSION['tiempo_inicio'][$_SESSION['pregunta_actual']];
+            $tiempo_transcurrido = $tiempo_actual - $tiempo_inicio_pregunta;
+            return $tiempo_transcurrido <= 10;
+        }
+        return false;
+    }
+
+    private function getPregunta()
+    {
+        // Verificar si hay preguntas disponibles
+        if (empty($_SESSION['preguntas_disponibles'])) {
+            return null; // No hay preguntas disponibles
         }
 
         $preguntas_disponibles = $_SESSION['preguntas_disponibles'];
@@ -46,59 +178,8 @@ class PartidaController
             $_SESSION['preguntas_disponibles'] = array_values($preguntas_disponibles);
         }
 
-        $datos['pregunta'] = $this->model->getPreguntaPorID($idGenerado);
-        $datos['respuesta'] = $this->model->getRespuestaPorID($idGenerado);
-
-        // Generar una clave única para esta pregunta
-        $pregunta_id = uniqid();
-
-        // Guardar el tiempo de inicio y la pregunta actual en la sesión
-        $_SESSION['tiempo_inicio'][$pregunta_id] = time();
-        $_SESSION['pregunta_actual'] = $pregunta_id;
-
-        $this->render->printView('jugarPartida', $datos);
-    }
-
-    public function pantallaPerdedor()
-    {
-        $datos['puntaje'] = $_SESSION['puntaje'];
-        $this->render->printView('pantallaPerdedor', $datos);
-    }
-    public function verificarRespuesta()
-    {
-        $datos = array();
-        if (isset($_POST['id'])) {
-            $id = $_POST['id'];
-     
-            $respuesta = $this->model->getRespuesta($id);
-     
-            $resultado = $respuesta[0]["es_correcta_int"];
-     
-            if ($resultado == '1') {
-                // Verificar si ha pasado menos de 10 segundos desde el inicio de la pregunta
-                if (isset($_SESSION['tiempo_inicio'][$_SESSION['pregunta_actual']])) {
-                    $tiempo_actual = time();
-                    $tiempo_inicio_pregunta = $_SESSION['tiempo_inicio'][$_SESSION['pregunta_actual']];
-                    if (($tiempo_actual - $tiempo_inicio_pregunta) <= 10) {
-                        $this->puntaje++;
-                        $_SESSION['puntaje'] += $this->puntaje;
-                        $usuario = $_SESSION['user'];
-                        $id = $this->model->getIdPartida($usuario);
-                        if ($id !== null) {
-                            $this->model->aumentarPuntuacionEnPartida($usuario, $id);
-                        } else {
-                            $this->model->subirPuntuacionEnPartida($usuario);
-                        }
-     
-                        $this->mostrarPantallaPartida();
-                    } else {
-                        $datos['puntaje'] = $_SESSION['puntaje'];
-                        $_SESSION['puntaje'] = $this->puntaje;
-                        $this->model->guardarPartida();
-                        $this->render->printView('pantallaPerdedor', $datos);
-                    }
-                }
-            }
-        }
+        return $idGenerado;
     }
 }
+    
+
--- model/PartidaModel.php
+++ model/PartidaModel.php
@@ -15,34 +15,34 @@ class PartidaModel
 
     public function getPreguntaYSuRespuesta()
     {
-        return $this->database->query(
-            'select * 
-        from (SELECT *
-        FROM pregunta
-        ORDER BY RAND()
-        LIMIT 1) a
-        left join respuesta
-        on a.id = respuesta.pregunta'
-        );
+        $query = 'SELECT p.id AS pregunta_id, p.descripcion AS pregunta_descripcion, c.tipo AS categoria_tipo, c.imagen AS categoria_imagen, r.id AS respuesta_id, r.texto AS respuesta_texto, r.es_correcta AS respuesta_correcta
+                  FROM pregunta p
+                  JOIN categoria c ON p.categoria = c.id
+                  JOIN respuesta r ON p.id = r.pregunta
+                  ORDER BY RAND()
+                  LIMIT 1';
+
+        return $this->database->query($query);
     }
+
 
 
     public function getRespuestas()
     {
         return $this->database->query('SELECT * FROM respuesta');
     }
- public function subirPuntuacionEnPartida($usuario)
+    public function subirPuntuacionEnPartida($usuario)
     {
         $this->database->queryB("UPDATE partida SET puntaje = puntaje + 1 where user_name like '" . $usuario . "'");
     }
-public function aumentarPuntuacionEnPartida($usuario, $id)
-{
-    if($id!==NULL)
-        $this->database->queryB("UPDATE partida SET puntaje = puntaje + 1 WHERE user_name = '" . $usuario . "' AND id = " . $id);
-    else{
-        $this->database->queryB("UPDATE partida SET puntaje = puntaje + 1 where user_name like '" . $usuario . "'");
-    }    
-}
+    public function aumentarPuntuacionEnPartida($usuario, $id)
+    {
+        if ($id !== NULL)
+            $this->database->queryB("UPDATE partida SET puntaje = puntaje + 1 WHERE user_name = '" . $usuario . "' AND id = " . $id);
+        else {
+            $this->database->queryB("UPDATE partida SET puntaje = puntaje + 1 where user_name like '" . $usuario . "'");
+        }
+    }
 
 
     public function getPreguntas()
@@ -72,27 +72,24 @@ public function aumentarPuntuacionEnPartida($usuario, $id)
         $query = 'SELECT CAST(es_correcta AS SIGNED) AS es_correcta_int FROM respuesta WHERE id = ' . $idRandom;
         return $this->database->query($query);
     }
-    public function guardarPartida(){
+    public function guardarPartida()
+    {
         $usuario = $_SESSION['user'];
         $puntaje = $_SESSION['puntaje'];
         $this->database->queryB("INSERT INTO partida(user_name, puntaje, fecha) VALUES('$usuario', '$puntaje', NOW())");
     }
-    public function getIdPartida($usuario){
+    public function getIdPartida($usuario)
+    {
         $query = "SELECT MAX(id) AS max_id FROM partida WHERE user_name = '" . $usuario . "'";
         $result = $this->database->query($query);
-    
+
         // if ($result !== false && $result->num_rows > 0) {
         //     $row = $result->fetch_assoc();
         //     if ($row) {
         //         return $row['max_id'];
         //     }
         // }
-    
-       return null;
 
+        return null;
     }
-    
-    
-    
-    
 }
