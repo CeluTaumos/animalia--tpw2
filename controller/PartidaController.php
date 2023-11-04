ca7082547c2a61780f4a0d3ce71d6021c7117b81
Axel Daniel <axeelleguero@gmail.com>
2023-11-04 00:07:03 -0300

    aaaa

--- controller/PartidaController.php
+++ controller/PartidaController.php
@@ -20,68 +20,62 @@ class PartidaController
     public function generarRandom()
     {
 
-        return rand(1, 22);
+       // return rand(1, 22);
     }
 
-public function seRecargo() {
-        foreach ($_SESSION['preguntas_mostradas']['id'] as $id) {
-            if (!in_array($id, $_SESSION['preguntas_respondidas'])) {
-                return false;
-            }
-        }
-        return true;
-    }
 
     public function mostrarPantallaPartida()
-{
-    if (!isset($_SESSION['preguntas_mostradas'])) {
-        $_SESSION['preguntas_mostradas'] = array();
+    {
+    
+        if (!isset($_SESSION['preguntas_mostradas'])) {
+            $_SESSION['preguntas_mostradas'] = array();
+        }
+
+        if (empty($_SESSION['preguntas_disponibles'])) {
+            $preguntas = $this->model->getPreguntas();
+            $_SESSION['preguntas_disponibles'] = array_column($preguntas, 'id');
+        }
+
+        $preguntaAnteriorId = $_SESSION['pregunta_actual'];
+
+        if ($preguntaAnteriorId) {
+
+            $tiempoInicioPreguntaAnterior = $_SESSION['tiempo_inicio'][$preguntaAnteriorId];
+            $tiempoActual = time();
+            $tiempoTranscurrido = $tiempoActual - $tiempoInicioPreguntaAnterior;
+
+
+            $this->puntaje++;
+            $_SESSION['puntaje'] += $this->puntaje;
+        }
+
+        $preguntas_disponibles = $_SESSION['preguntas_disponibles'];
+        $idGenerado = $preguntas_disponibles[array_rand($preguntas_disponibles)];
+
+        $_SESSION['preguntas_mostradas'][] = $idGenerado;
+
+        $key = array_search($idGenerado, $preguntas_disponibles);
+        if ($key !== false) {
+            unset($preguntas_disponibles[$key]);
+            $_SESSION['preguntas_disponibles'] = array_values($preguntas_disponibles);
+        }
+
+        $datos['pregunta'] = $this->model->getPreguntaPorID($idGenerado);
+        $datos['respuesta'] = $this->model->getRespuestaPorID($idGenerado);
+
+        $pregunta_id = uniqid();
+
+        $_SESSION['tiempo_inicio'][$pregunta_id] = time();
+        $_SESSION['pregunta_actual'] = $pregunta_id;
+
+        $this->render->printView('jugarPartida', $datos);
+        if ($tiempoTranscurrido > 10 && !isset($_SESSION['pantalla_perdedor_mostrada'])) {
+            $_SESSION['pantalla_perdedor_mostrada'] = true;
+            $this->pantallaPerdedor();
+            return;
+        }
+
     }
-
-    if (empty($_SESSION['preguntas_disponibles'])) {
-        $preguntas = $this->model->getPreguntas();
-        $_SESSION['preguntas_disponibles'] = array_column($preguntas, 'id');
-    }
-
-    $preguntaAnteriorId = $_SESSION['pregunta_actual'];
-
-    if ($preguntaAnteriorId) {
-        
-        $tiempoInicioPreguntaAnterior = $_SESSION['tiempo_inicio'][$preguntaAnteriorId];
-        $tiempoActual = time();
-        $tiempoTranscurrido = $tiempoActual - $tiempoInicioPreguntaAnterior;
-
-       
-        $this->puntaje++;
-        $_SESSION['puntaje'] += $this->puntaje;
-    }
-
-    $preguntas_disponibles = $_SESSION['preguntas_disponibles'];
-    $idGenerado = $preguntas_disponibles[array_rand($preguntas_disponibles)];
-
-    $_SESSION['preguntas_mostradas'][] = $idGenerado;
-
-    $key = array_search($idGenerado, $preguntas_disponibles);
-    if ($key !== false) {
-        unset($preguntas_disponibles[$key]);
-        $_SESSION['preguntas_disponibles'] = array_values($preguntas_disponibles);
-    }
-
-    $datos['pregunta'] = $this->model->getPreguntaPorID($idGenerado);
-    $datos['respuesta'] = $this->model->getRespuestaPorID($idGenerado);
-
-    $pregunta_id = uniqid();
-
-    $_SESSION['tiempo_inicio'][$pregunta_id] = time();
-    $_SESSION['pregunta_actual'] = $pregunta_id;
-
-    $this->render->printView('jugarPartida', $datos);
-    if ($tiempoTranscurrido > 10) {
-        $this->pantallaPerdedor();
-        return;
-    }
-
-}
 
     //VERIFICACION DE LO QUE EL USUARIO RESPONDE 
 
@@ -90,18 +84,20 @@ public function seRecargo() {
         $datos = array();
         if (isset($_POST['id'])) {
             $id = $_POST['id'];
-    
+
             $respuesta = $this->model->getRespuesta($id);
-    
+
             $resultado = $respuesta[0]["es_correcta_int"];
-    
+
+            $_SESSION['preguntas_respondidas'][] = $id;
+
             if ($resultado == '1') {
                 $preguntaActualId = $_SESSION['pregunta_actual'];
                 if ($preguntaActualId && isset($_SESSION['tiempo_inicio'][$preguntaActualId])) {
                     $tiempoActual = time();
                     $tiempoInicioPregunta = $_SESSION['tiempo_inicio'][$preguntaActualId];
                     $tiempoTranscurrido = $tiempoActual - $tiempoInicioPregunta;
-    
+
                     if ($tiempoTranscurrido <= 10) {
                         // El usuario respondió correctamente en menos de 10 segundos, se suma puntaje
                         $this->puntaje++;
@@ -113,19 +109,19 @@ public function seRecargo() {
                         } else {
                             $this->model->subirPuntuacionEnPartida($usuario);
                         }
-    
-                    
+
+
                         $this->mostrarPantallaPartida();
                         return;
                     }
                 }
-            }
+            } $this->pantallaPerdedor(); return;
         }
-    
-   
+
         $this->pantallaPerdedor();
-    }
     
+    }
+
 
     public function pantallaPerdedor()
     {
