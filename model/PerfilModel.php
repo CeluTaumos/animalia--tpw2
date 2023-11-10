1eb59ec278c08095c80a2cd5019bc7e45b89af60
Axel Daniel <axeelleguero@gmail.com>
2023-11-10 16:52:51 -0300

    Vista modificar preguntas lista

--- model/PerfilModel.php
+++ model/PerfilModel.php
@@ -19,4 +19,30 @@ class PerfilModel{
         $query = "SELECT * FROM usuario WHERE user_name =  '$user'";
         return $this->database->query($query);
     }
+    public function agregarCategoria($categoria){
+        $query = "INSERT INTO categoriaSugerida (tipo) 
+              VALUES ('$categoria')";
+        $resultado =$this->database->queryB($query);
+    }
+    public function agregarRespuestas($respuestas){
+        $query = "INSERT INTO respuestasSugeridas (descripcion) VALUES ()";
+        $resultado =$this->database->queryB($query);
+    }
+
+    public function obtenerPreguntas() {
+        $query = "SELECT descripcion FROM pregunta";
+        $result = $this->database->queryB($query);
+        if ($result && $result instanceof mysqli_result) {
+            $preguntas = array();
+            while ($row = $result->fetch_assoc()) {
+                $preguntas[] = array('descripcion' => $row['descripcion']);
+            }
+            return array('pregunta' => $preguntas);
+        } else {
+            
+            return array('pregunta' => array()); 
+        }
+    }
+    
 }
+
