e589773e24daf71a19b15ded0716f17b9217184b
LudmiAbril <ludmila.pereyra543@gmail.com>
2023-11-10 12:50:57 -0300

    registro precioso terminado u.u

--- controller/AnimaliaController.php
+++ controller/AnimaliaController.php
@@ -1,4 +1,7 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/autoload.php'; // Asegúrate de que la ruta sea correcta según la estructura de tu proyecto
+
 class AnimaliaController
 {
     private $render;
@@ -16,63 +19,122 @@ class AnimaliaController
     }
     public function procesarFormulario()
     {
-        
+        $datos = null;
+        // guardo los datos en variables para trabaajr con ellas
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $usuario = $_POST["username"];
-            $password= $_POST["pass"];
-            $nombre= $_POST["nombre"];
-            $fecha= $_POST["fecha"];
-            $sexo= $_POST["nombre"];
-            $mail= $_POST["user"];
-            $imagen = $_POST["file"];
-            if (!empty($usuario) && !empty($password) && !empty($nombre) && !empty($fecha) && !empty($sexo) && !empty($mail)) {
-                $this->model->registrarUsuario($usuario, $password, $nombre, $fecha, $sexo, $mail);
-                if (isset($_FILES["file"])) {
-                    $nombreArchivo = $_FILES["file"]["name"];
-                    $rutaTemporal = $_FILES["file"]["tmp_name"];
-                    $directorioDestino = "config/images/" . $nombreArchivo;
-        
-                    if (move_uploaded_file($rutaTemporal, $directorioDestino)) {
-                        $imagen = $directorioDestino;
-                    } else {
-                        echo "Error al subir la imagen.";
+            $password = $_POST["pass"];
+            $r_password = $_POST["r_pass"];
+            $nombre = $_POST["nombre"];
+            $fecha = $_POST["fecha"];
+            $sexo = $_POST["sexo"];
+            $mail = $_POST["email"];
+            $imagen = $_FILES["file"];
+
+            // valido que nada este vacio
+            if (!empty($usuario) && !empty($password) && !empty($r_password) && !empty($nombre) && !empty($fecha) && !empty($sexo) && !empty($mail)) {
+
+                /* valido todo, subo la imagen, si la imagen ya se subio, registro 
+                al ususario y mando el mail */
+
+                if ($this->usuarioDisponible($usuario, $datos) && $this->contraseñaValida($password, $r_password, $datos)) {
+
+                    if (isset($_FILES["file"])) {
+                        $nombreArchivo = $_FILES["file"]["name"];
+                        $rutaTemporal = $_FILES["file"]["tmp_name"];
+                        $directorioDestino = "config/images/" . $nombreArchivo;
+
+                        if (move_uploaded_file($rutaTemporal, $directorioDestino)) {
+                            $imagen = $directorioDestino;
+                            $this->model->registrarUsuario($usuario, $password, $nombre, $fecha, $sexo, $mail, $imagen);
+                            // $this->model->subirFoto($usuario, $imagen);
+                            $this->enviarCorreoConfirmacion($mail,$nombre);
+                            $datos['mensaje-registro-exitoso'] = "Tu registro fue exitoso, en breves recibiras un mail con la confirmacion";
+                        } else {
+                            $datos['mensaje-error-registro'] = "Error al subir la imagen";
+                        }
                     }
                 }
-                $this->model->subirFoto($usuario, $imagen);
-                $this->enviarCorreo();
+            } else {
+                $datos['mensaje-error-registro'] = 'los campos no pueden estar vacios';
             }
-        $datos = null;
-        $this->render->printView('lobby', $datos);
+            $this->render->printView('index', $datos);
+        }
     }
-}
-    public function validarCorreo(){
-        $datos=null;
+
+
+    public function validarLoginUsuario()
+    {
+        $datos = null;
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $usuario = $_POST["username"];
-            $password= $_POST["pass"];
+            $password = $_POST["pass"];
             $datosObtenidos = $this->model->revisarUsuarioYPass($usuario, $password);
 
-            if(!empty($datosObtenidos)){
+            if (!empty($datosObtenidos)) {
                 //para pasar los datos a mustache, debo guardar datos con el nombre entre []
                 $datos['user'] = $datosObtenidos[0]['user_name'];
                 $_SESSION['user'] = $usuario;
-                $_SESSION['puntaje'] =0;
+                $_SESSION['puntaje'] = 0;
                 $datos['puntaje'] = $_SESSION['puntaje'];
                 $this->render->printView('lobby', $datos);
-            }else{
+            } else {
                 $this->render->printView('index', $datos);
             }
-            
+
+        }
+
+
+    }
+
+
+
+    public function usuarioDisponible($usuario, &$datos)
+    {
+        if ($this->model->usuarioRepetido($usuario)) {
+            $datos['mensaje-error-registro'] = 'este nombre de usuario ya existe, elija otro';
+            return false;
+        }
+        return true;
+    }
+
+    public function contraseñaValida($contra, $contraR, &$datos)
+    {
+        if ($contra == $contraR) {
+            return true;
+        } else {
+            // Aquí estableces el mensaje de error específico para la contraseña
+            $datos['mensaje-error-registro'] = 'Las contraseñas no coinciden.';
+            return false;
         }
     }
-    public function enviarCorreo()
+
+    public function enviarCorreoConfirmacion($correoDestinatario, $nombreDestinatario)
     {
-        if (isset($_POST["enviar"])) {
 
-            //  if ($_POST["user"] == "mica" && $_POST["pw"] == 123) {
 
-            //$_SESSION["user"] = "user";
-            //mica-axel-ludmi-cele--MALC *^____^*
-        }
+        // Instancia de PHPMailer
+        $mail = new PHPMailer;
+
+        // Configuración del servidor SMTP
+        $mail->isSMTP();
+        $mail->Host = 'smtp-mail.outlook.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'animaliaJuego@hotmail.com'; // Tu correo electrónico
+        $mail->Password = 'animalia1234'; // Tu contraseña
+        $mail->SMTPSecure = 'tls'; // O ssl si corresponde
+        $mail->Port = 587; // El puerto puede variar, verifica con tu proveedor de correo
+
+        // Configuración del correo
+        $mail->setFrom('animaliaJuego@hotmail.com', 'Animalia');
+        $mail->addAddress($correoDestinatario, $nombreDestinatario); // $mail contiene el correo del usuario, definido en tu función
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Registro exitoso';
+        $mail->Body = 'Hola! bienvenido a Animalia, que te diviertas :) 
+        - MALC owo';
+
+        //mica-axel-ludmi-cele--MALC *^____^*
+
     }
 }
--- model/AnimaliaModel.php
+++ model/AnimaliaModel.php
@@ -1,38 +1,51 @@
 <?php
 
-class AnimaliaModel {
+class AnimaliaModel
+{
     private $database;
 
-    public function __construct($database) {
+    public function __construct($database)
+    {
         $this->database = $database;
     }
 
-    public function revisarUsuarioYPass($usuario, $password){
+    public function revisarUsuarioYPass($usuario, $password)
+    {
         $query = ("SELECT user_name, contrasenia FROM usuario WHERE user_name = '$usuario' AND contrasenia = '$password'");
         return $this->database->query($query);
     }
-    public function registrarUsuario($usuario, $password, $nombre, $fecha, $sexo, $mail){
-        $query = "INSERT INTO usuario (user_name, contrasenia, nombre_completo, anio_de_nacimiento, sexo, mail) 
-              VALUES ('$usuario', '$password', '$nombre', '$fecha', '$sexo', '$mail')";
-        $resultado =$this->database->query($query);
-        if ($resultado) {
-            echo "Usuario registrado con éxito.";
-        } else {
-            echo "Error al registrar el usuario: " . mysqli_error($this->database);
+    public function registrarUsuario($usuario, $password, $nombre, $fecha, $sexo, $mail, $foto)
+    {
+        $query = "INSERT INTO usuario (user_name, contrasenia, nombre_completo, anio_de_nacimiento, sexo, mail, foto_de_perfil, rol, nivel) 
+              VALUES ('$usuario', '$password', '$nombre', '$fecha', '$sexo', '$mail' , '$foto' , 'jugador', 'principiante')";
+        $resultado = $this->database->queryB($query);
+        
+    }
+
+    public function subirFoto($usuario, $imagen)
+    {
+        if (!empty($imagen)) {
+            $query = "UPDATE usuario
+        SET foto_de_perfil = '$imagen'
+        WHERE user_name = '$usuario'";
+            $resultado = $this->database->query($query);
+            if ($resultado) {
+                echo "Foto subida con éxito.";
+            } else {
+                echo "Error al subir la foto: " . mysqli_error($this->database);
+            }
         }
     }
 
-    public function subirFoto($usuario, $imagen){
-        if (!empty($imagen)) {
-        $query = "UPDATE usuario
-        SET foto_de_perfil = '$imagen'
-        WHERE user_name = '$usuario'";
+    public function usuarioRepetido($usuario)
+    {
+        $query = "SELECT * FROM usuario WHERE user_name = '" . $usuario . "'";
         $resultado = $this->database->query($query);
-        if ($resultado) {
-            echo "Foto subida con éxito.";
+        if ($resultado == null) {
+            return false;
         } else {
-            echo "Error al subir la foto: " . mysqli_error($this->database);
-  }
+            return true;
+        }
+
     }
-}
 }
