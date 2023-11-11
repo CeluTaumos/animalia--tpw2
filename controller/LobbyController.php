89285f4a489b03a76cec39086c2124fc2dfd17a1
Axel Daniel <axeelleguero@gmail.com>
2023-11-11 18:46:21 -0300

    Error de redireccion a lobby editor fix

--- controller/LobbyController.php
+++ controller/LobbyController.php
@@ -17,9 +17,22 @@ class LobbyController
 
     public function mostrarPantallaLobby()
     {
-        $datos['puntaje'] = $_SESSION['puntaje'];
-        $datos['user'] = $_SESSION['user'];
-        $this->render->printView('lobby', $datos);
+        $rol = $_SESSION['rol'];
+        $datos = null;
+        if ($rol == 'admin') {
+            //Aca deberia haber un metodo que le pase los datos de las estadisticas a lobbyAdmin
+            $_SESSION['estadisticas'] = $this->model->obtenerEstadisticas();
+            $datos = $_SESSION['estadisticas'];
+            $this->render->printView('lobbyadmin', $datos);
+        } elseif ($rol == 'editor') {
+            $datos['user'] = $_SESSION['user'];
+            $this->render->printView('lobbyeditor', $datos);
+        }else{
+            $datos['puntaje'] = $_SESSION['puntaje'];
+            $datos['user'] = $_SESSION['user'];
+            $this->render->printView('lobby', $datos);
+        }
+        
     }
 
     public function cerrarSesion()
