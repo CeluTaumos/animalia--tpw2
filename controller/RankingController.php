6baba255fa0ac365269beb0c90df49fbcf327916
Micaela Zara <zara_micaela@outlook.com>
2023-11-11 13:07:52 -0300

    estoy harta locooo

--- controller/RankingController.php
+++ controller/RankingController.php
@@ -1,42 +1,47 @@
 <?php
-class RankingController{
+class RankingController
+{
     private $render;
     private $model;
 
-    public function __construct($render, $model) {
+    public function __construct($render, $model)
+    {
         $this->render = $render;
         $this->model = $model;
     }
-    public function mostrarPantallaRanking(){
-        $datos['partida']=$this->model->getRanking();
-        $datos['partidasPrevias']=$this->model->getPartidasPrevias();
+    public function mostrarPantallaRanking()
+    {
+        $datos['partida'] = $this->model->getRanking();
+        $datos['partidasPrevias'] = $this->model->getPartidasPrevias();
         $datos['user'] = $_SESSION['user'];
         $this->render->printView('ranking', $datos);
     }
-    public function imprimirQR() {
+    public function imprimirQR()
+    {
         //$datos = "Nombre:Cacho";
         //QRcode::png($datos,false,QR_ECLEVEL_L,8);
-        if(isset($_POST['enviar'])){
+        if (isset($_POST['enviar'])) {
             $usuario = $_POST['user'];
-            
         }
-        
+
         //QRcode::png('xdlfjhsdfljghsfd','cacho.png');
         $datosUsuario = $this->model->getUsuario($usuario);
-        
+
         // Construye la URL con el parámetro del usuario
         $url = "http://localhost/Perfil/mostrarPantallaPerfilNuevo?usuario=" . urlencode($usuario);
-    
+
         // Genera el código QR con la URL
         //include('phpqrcode/qrlib.php');
         QRcode::png($url, 'public/qr.png', 'L', 4);
-    
+
         // Paso los datos del usuario y la URL del código QR a la vista
         $datos['usuario'] = $datosUsuario;
         $datos['urlQR'] = $url;
-    
-        $this->render->printView('perfilEstatico', $datos);
+        // Devolver los datos en formato JSON para que lo tome el ajax
+        echo json_encode($datos);
+        exit;
+        // $this->render->printView('perfilEstatico', $datos);
     }
-    
-    
+
+
 }
