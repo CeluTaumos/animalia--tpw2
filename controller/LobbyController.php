678d87fe4f4245978212e858aecd29d894605100
Celena Moscovich <celena.ark@gmail.com>
2023-11-13 21:18:33 -0300

    renderizacion nivel

--- controller/LobbyController.php
+++ controller/LobbyController.php
@@ -16,24 +16,33 @@ class LobbyController
     }
 
     public function mostrarPantallaLobby()
-    {
-        $rol = $_SESSION['rol'];
-        $datos = null;
-        if ($rol == 'admin') {
-            //Aca deberia haber un metodo que le pase los datos de las estadisticas a lobbyAdmin
-            $_SESSION['estadisticas'] = $this->model->obtenerEstadisticas();
-            $datos = $_SESSION['estadisticas'];
-            $this->render->printView('lobbyadmin', $datos);
-        } elseif ($rol == 'editor') {
-            $datos['user'] = $_SESSION['user'];
-            $this->render->printView('lobbyeditor', $datos);
-        }else{
+{
+    $rol = $_SESSION['rol'];
+    $datos = null;
+    $user = $_SESSION['user'];
+
+    if ($rol == 'admin') {
+        // Aca deberia haber un metodo que le pase los datos de las estadisticas a lobbyAdmin
+        $_SESSION['estadisticas'] = $this->model->obtenerEstadisticas();
+        $datos = $_SESSION['estadisticas'];
+        $this->render->printView('lobbyadmin', $datos);
+    } elseif ($rol == 'editor') {
+        $datos['user'] = $_SESSION['user'];
+        $this->render->printView('lobbyeditor', $datos);
+    } else {
+        $userResult = $this->model->getUser($user);
+        
+        if ($userResult) {
+            $userData = $userResult->fetch_assoc(); // Suponiendo que estás utilizando MySQLi
             $datos['puntaje'] = $_SESSION['puntaje'];
             $datos['user'] = $_SESSION['user'];
-            $this->render->printView('lobby', $datos);
+            $datos['nivel'] = $userData['nivel'];
         }
         
+        $this->render->printView('lobby', $datos);
     }
+}
+
 
     public function cerrarSesion()
     {
