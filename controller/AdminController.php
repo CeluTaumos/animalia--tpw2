1f15d097d15cdea81eb62c3a46900927dd51c28c
Axel Daniel <axeelleguero@gmail.com>
2023-11-16 14:29:05 -0300

    Grafico de usuarios en el mundo

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -2,6 +2,7 @@
 require_once ('./third-party/jpgraph/src/lib/jpgraph.php');
 require_once ('./third-party/jpgraph/src/lib/jpgraph_bar.php');
 require_once ('./third-party/jpgraph/src/lib/jpgraph_pie.php');
+require_once ('./third-party/jpgraph/src/lib/jpgraph_pie3d.php');
 class AdminController
 {
     private $render;
@@ -165,4 +166,45 @@ tablas de datos)*/
         $p1->SetSliceColors(array('#1E90FF','#BA55D3','#2E8B57'));//,'#DC143C','#BA55D3', 
         $graph->Stroke();
     }
+    public function graficoMundial(){
+        $data = array(40,60,21,33);
+        $piepos = array(0.2,0.35,0.6,0.28,0.3,0.7,0.85,0.7);
+        $titles = array('USA','Sweden','South America','Australia');
+
+        $n = count($piepos)/2;
+        // A new graph
+        $graph = new PieGraph(450,300,'auto');
+        $theme_class="VividTheme";
+        $graph->SetTheme(new $theme_class());
+        // Setup background
+        $graph->SetBackgroundImage('./public/world_map.png',BGIMG_FILLFRAME);
+        // Setup title
+        $graph->title->Set("Usuarios en el mundo");
+        $graph->title->SetFont(FF_DEFAULT, FS_BOLD);
+        $graph->title->SetColor('white');
+        $graph->SetTitleBackground('#004466@0.3',TITLEBKG_STYLE2,TITLEBKG_FRAME_FULL,'#004466@0.3',10,10,true);
+
+        $p = array();
+        // Position the four pies and change color
+        for( $i=0; $i < $n; ++$i ) {
+            $p[$i] = new PiePlot3D($data);
+            $graph->Add($p[$i]);
+
+            $p[$i]->SetCenter($piepos[2*$i],$piepos[2*$i+1]);
+
+            // Set the titles
+            $p[$i]->title->Set($titles[$i]);
+            $p[$i]->title->SetFont(FF_DEFAULT, FS_BOLD, 10);
+            $p[$i]->title->SetColor('white');
+
+            // Size of pie in fraction of the width of the graph
+            $p[$i]->SetSize(0.17);
+            $p[$i]->SetHeight(8);
+            $p[$i]->SetEdge(false);
+            $p[$i]->ExplodeSlice(1,7);
+            $p[$i]->value->Show(false);
+        }
+        $graph->SetAntiAliasing(false);
+        $graph->Stroke();
+            }
 }
